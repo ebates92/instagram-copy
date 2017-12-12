4900e7e3935977a06703edba97be73fe55ddef7c
Evan Bates <ebates92@gmail.com>
2017-12-12 14:40:19 -0500

    finally I was able to get the search bar to work and render the page again with new items

--- scripts/main.js
+++ scripts/main.js
@@ -1,108 +1,159 @@
-// example
 
-// var DATA_SELECTOR = '[data-goes-here]"'
+var HOUSEDATADICTIONARY = [       
+        {
+            "image": "images/house_1.jpg",
+            "street" :"151 Hale St NE,",
+            "cityState":"Atlanta, GA 30307",
+            "size":"3 beds / 2 baths / 1,903 sqft",
+            "price":"$679,900"
+        },
+    
+        {
+            "image":"images/house_2.jpg",
+            "street":"2017 Palifox Dr NE,",
+            "cityState":"Atlanta, GA 30307",
+            "size":"3 beds / 2 baths / 1,878 sqft",
+            "price":"$589,000"
+        },
+    
+        {
+            "image": "images/house_3.jpg",
+            "street":"36 Leslie St NE,",
+            "cityState":"Atlanta, GA 30307",
+            "size":"4 beds / 2 baths / 1,900 sqft",
+            "price":"$429,900"
+        }, 
+    
+        {
+            "image": "images/house_4.jpg",
+            "street":"491 Harold Ave NE,",
+            "cityState":"Atlanta, GA 30307",
+            "size":"3 beds / 2 baths / 1,424 sqft",
+            "price":"$449,900"
+        }, 
+    
+        {
+            "image": "images/house_5.jpg",
+            "street":"1892 Gordon Mnr NE,",
+            "cityState":"Atlanta, GA 30307",
+            "size":"2 beds / 2 baths / --sqft",
+            "price":"$389,900"
+        }, 
+    
+        {
+            "image": "images/house_6.jpg",
+            "street":"209 Lampkin St NE,",
+            "cityState":"Atlanta, GA 30307",
+            "size":"1 beds / 2 baths / 1,224 sqft",
+            "price":"$365,000"
+        },
+        {
+            "image": "images/house_7.jpg",
+            "street":"1124 Faith Ave SE,",
+            "cityState":"Atlanta, GA 30307",
+            "size":"3 beds / 3 baths / 1,586 sqft",
+            "price":"$415,000"
+        }, 
+        {
+            "image":"images/house_8.jpg",
+            "street":"1741 S Ponce De Leon Ave NE,",
+            "cityState":"Atlanta, GA 30307",
+            "size":"4 beds / 5 baths / 5,400 sqft",
+            "price":"$2,700,000"
+        }, 
+        {
+            "image": "images/house_9.jpg",
+            "street":"860 Peachtree St NE Unit 713,",
+            "cityState":"Atlanta, GA 30307",
+            "size":"1 beds / 1 baths / 1,060 sqft",
+            "price":"$390,000"
+        }, 
+        {
+            "image": "images/house_10.jpg",
+            "street":"855 Peachtree St NE Unit 3209,",
+            "cityState":"Atlanta, GA 30307",
+            "size":"1 beds / 2 baths / 1,000 sqft",
+            "price":"$375,000"
+        },
+        {
+            "image": "images/house_11.jpg",
+            "street":"1065 Peachtree St NE Unit 3005,",
+            "cityState":"Atlanta, GA 30307",
+            "size":"2 beds / 3 baths / 2,289 sqft",
+            "price":"$1,325,000"
+        }, 
+        {  
+            "image": "images/house_12.jpg",
+            "street":"1419 Lanier Pl NE,",
+            "cityState":"Atlanta, GA 30307",
+            "size":"4 beds / 4 baths / 3,500 sqft",
+            "price":"$1,275,000"
+        }                
+    ]
 
-// var dataVariableToBeUsed = document.querySelector(data-goes-here);
+var SERVER_URL = '/house.json'
 
-var URL = 'http://dc-coffeerun.herokuapp.com/api/coffeeorders'
-var THEDATA = $.get(URL, function (data) {
-    return data;
-});
 
-var HOUSEDATADICTIONARY = {
-    "images/house_1.jpg" : {
-        'street' :'151 Hale St NE,',
-        'cityState':'Atlanta, GA 30307',
-        'size':'3 beds / 2 baths / 1,903 sqft',
-        'price':'$679,900'
-    },
-    "images/house_2.jpg" : {
-        'street':'2017 Palifox Dr NE,',
-        'cityState':'Atlanta, GA 30307',
-        'size':'3 beds / 2 baths / 1,878 sqft',
-        'price':'$589,000'
-    },
-    "images/house_3.jpg" : {
-        'street':'36 Leslie St NE,',
-        'cityState':'Atlanta, GA 30307',
-        'size':'4 beds / 2 baths / 1,900 sqft',
-        'price':'$429,900'
-    }, 
-    "images/house_4.jpg" : {
-        'street':'491 Harold Ave NE,',
-        'cityState':'Atlanta, GA 30307',
-        'size':'3 beds / 2 baths / 1,424 sqft',
-        'price':'$449,900'
-    }, 
-    "images/house_5.jpg" : {
-        'street':'1892 Gordon Mnr NE,',
-        'cityState':'Atlanta, GA 30307',
-        'size':'2 beds / 2 baths / --sqft',
-        'price':'$389,900'
-    }, 
-    "images/house_6.jpg" : {
-        'street':'209 Lampkin St NE,',
-        'cityState':'Atlanta, GA 30307',
-        'size':'1 beds / 2 baths / 1,224 sqft',
-        'price':'$365,000'
-    },
-    "images/house_7.jpg" : {
-        'street':'1124 Faith Ave SE,',
-        'cityState':'Atlanta, GA 30307',
-        'size':'3 beds / 3 baths / 1,586 sqft',
-        'price':'$415,000'
-    }, 
-    "images/house_8.jpg" : {
-        'street':'1741 S Ponce De Leon Ave NE,',
-        'cityState':'Atlanta, GA 30307',
-        'size':'4 beds / 5 baths / 5,400 sqft',
-        'price':'$2,700,000'
-    }, 
-    "images/house_9.jpg" : {
-        'street':'860 Peachtree St NE Unit 713,',
-        'cityState':'Atlanta, GA 30307',
-        'size':'1 beds / 1 baths / 1,060 sqft',
-        'price':'$390,000'
-    }, 
-    "images/house_10.jpg" : {
-        'street':'855 Peachtree St NE Unit 3209,',
-        'cityState':'Atlanta, GA 30307',
-        'size':'1 beds / 2 baths / 1,000 sqft',
-        'price':'$375,000'
-    },
-    "images/house_11.jpg" : {
-        'street':'1065 Peachtree St NE Unit 3005,',
-        'cityState':'Atlanta, GA 30307',
-        'size':'2 beds / 3 baths / 2,289 sqft',
-        'price':'$1,325,000'
-    },   
-    "images/house_12.jpg" : {
-        'street':'1419 Lanier Pl NE,',
-        'cityState':'Atlanta, GA 30307',
-        'size':'4 beds / 4 baths / 3,500 sqft',
-        'price':'$1,275,000'
-    },                 
+
+function main() {
+    window.onload = function () {
+        addFrontPageImages()
+
+            .then(createPhotoPopOut)
+    }
 };
 
-var HOUSEKEYS = Object.keys(HOUSEDATADICTIONARY);
+function useSearchFilter (string) {
+    var newHouseDataDictionary = HOUSEDATADICTIONARY.filter(function (thing){
+        return stringSearch(thing, string);
+    });
+    // document.querySelector('.container').removeChild('[data-image]');
+    $('.container').empty();
+    addFrontPageImages(newHouseDataDictionary);
+}
 
-window.onload = addFrontPageImages();
+function stringSearch (thing, string) {
+    var valuesArray = Object.values(thing)  
+    for (var x = 0; x < valuesArray.length; x++) {
+        var trueTest = valuesArray[x].search(string);
+        if (trueTest >= 0) {
+            return true;
+        }
+    }
+    return false;
+};
 
-function addFrontPageImages () {
+function captureSearchFilter (HOUSEDATADICTIONARY) {
+    var $searchBar = $('.nav input');
+    $searchBar.keydown(function(event) {
+        var $searchBar = $('.nav input')
+        if (event.which == '13') {
+            event.preventDefault();
+            var $filter = $searchBar.val();
+            useSearchFilter($filter);
+        }
+    });
+};
+
+captureSearchFilter();
+
+window.onload = addFrontPageImages(HOUSEDATADICTIONARY);
+
+function addFrontPageImages (houseArray) {
 
     var imageContainer = document.querySelector('.container');
 
-    HOUSEKEYS.forEach(function (house,index, array) {
+    houseArray.forEach(function (house,index, array) {
 
         // creates anchor tags for images
         var originalImageAnchor = document.createElement('a');
         imageContainer.appendChild(originalImageAnchor);
-        originalImageAnchor.setAttribute('src', house);
+        originalImageAnchor.setAttribute('src', house["image"]);
 
         // creates front page images
         var originalImage = document.createElement('img');
         originalImageAnchor.appendChild(originalImage);
-        originalImage.setAttribute('src', house);
+        originalImage.setAttribute('src', house["image"]);
         originalImage.setAttribute('alt', "house photo");
         originalImage.setAttribute('data-image', '');
 
@@ -113,15 +164,16 @@ function addFrontPageImages () {
 
         // creates header in image div
         var streetHeader = document.createElement('h2');
-        streetHeader.innerText = HOUSEDATADICTIONARY[house]['street'];
+        streetHeader.innerText = house['street'];
         nestedImageContainer.appendChild(streetHeader);
 
         // creates header in image div
         var valueHeader = document.createElement('h2');
-        valueHeader.innerText = HOUSEDATADICTIONARY[house]['price'];
+        valueHeader.innerText = house['price'];
         valueHeader.setAttribute('id','price');
         nestedImageContainer.appendChild(valueHeader);
     });
+    createPhotoPopOut();
 };
 
 // This creates a pop-up picture window in the DOM
@@ -133,7 +185,6 @@ function createPhotoPopOut () {
     photoArray.forEach(function (photo,index,array) {
         photo.addEventListener('click',function(clicked) {
             clicked.preventDefault();
-            // createPictureObjects (photo.src, photo.alt);
             var theImage = createAnImage(photo.src, photo.alt);
             var thePictureContainer = createPictureContainer();
             var thePictureWindow = createPictureWindow ();
@@ -143,7 +194,7 @@ function createPhotoPopOut () {
             var infoDiv = createPopUpDivContainer();
             fixedBody.classList.add('fixed');
             document.body.appendChild(thePictureContainer).appendChild(thePictureWindow).appendChild(theImage);
-            thePictureContainer.appendChild(infoDiv);
+            thePictureWindow.appendChild(infoDiv);
             thePictureContainer.appendChild(thePictureX);
             thePictureContainer.appendChild(scrollLeft);
             thePictureContainer.appendChild(scrollRight);
@@ -152,7 +203,6 @@ function createPhotoPopOut () {
         });
     });
 };
-
 
 function createPictureObjects (src, alt) {
     var theImage = createAnImage(src, alt);
@@ -237,16 +287,24 @@ function scrollImage (index,array) {
     scrollRight.addEventListener('click', function(clicked) {
         clicked.preventDefault();
         var newPosition = index + 1;
+        if (index === (HOUSEDATADICTIONARY.length-1)) {
+            newPosition = 0;
+        };
         var newImage = array[newPosition];
         currentImage.setAttribute('src',newImage.getAttribute('src'));
         index = newPosition;
+        updateHouseInfo(index);
     });
     scrollLeft.addEventListener('click', function(clicked) {
         clicked.preventDefault();
         var newPosition = index - 1;
+        if (index === 0) {
+            newPosition = (HOUSEDATADICTIONARY.length-1);
+        };
         var newImage = array[newPosition];
         currentImage.setAttribute('src',newImage.getAttribute('src'));
         index = newPosition;
+        updateHouseInfo(index);
     });
 
     // adds arrow scroll functionality
@@ -259,16 +317,24 @@ function scrollImage (index,array) {
         if (e.keyCode == '37') {
             // left arrow
             var newPosition = index - 1;
+            if (index === 0) {
+                newPosition = (HOUSEDATADICTIONARY.length-1);
+            };
             var newImage = array[newPosition];
             currentImage.setAttribute('src',newImage.getAttribute('src'));
             index = newPosition;
+            updateHouseInfo(index);
         }
         else if (e.keyCode == '39') {
             // right arrow
             var newPosition = index + 1;
+            if (index === (HOUSEDATADICTIONARY.length-1)) {
+                newPosition = 0;
+            };
             var newImage = array[newPosition];
             currentImage.setAttribute('src',newImage.getAttribute('src'));
             index = newPosition;
+            updateHouseInfo(index);
         };
     };
     createHouseInfo(index);
@@ -282,7 +348,7 @@ function createHouseInfo (index) {
     var $streetHeader = $('<h1>', {
         'class': 'address'
     });
-    $streetHeader.text(HOUSEDATADICTIONARY[HOUSEKEYS[index]]['street']);
+    $streetHeader.text(HOUSEDATADICTIONARY[index]['street']);
     $contentContainer.append($streetHeader);
 
 
@@ -290,23 +356,43 @@ function createHouseInfo (index) {
     var $cityStateHeader = $('<h1>', {
         'class': 'address2'
     });
-    $cityStateHeader.text(HOUSEDATADICTIONARY[HOUSEKEYS[index]]['cityState']);
+    $cityStateHeader.text(HOUSEDATADICTIONARY[index]['cityState']);
     $contentContainer.append($cityStateHeader);
 
     // // add house details
     var $sizeHeader = $('<h1>', {
         'class': 'size'
     });
-    $sizeHeader.text(HOUSEDATADICTIONARY[HOUSEKEYS[index]]['size']);
+    $sizeHeader.text(HOUSEDATADICTIONARY[index]['size']);
     $contentContainer.append($sizeHeader);
 
     // // add price
     var $priceHeader = $('<h1>', {
         'id': 'price2'
     });
-    $priceHeader.text(HOUSEDATADICTIONARY[HOUSEKEYS[index]]['price']);
+    $priceHeader.text(HOUSEDATADICTIONARY[index]['price']);
     $contentContainer.append($priceHeader);    
     
 };
 
-createPhotoPopOut();
+function updateHouseInfo (index) {
+
+    var $contentContainer = $('.popup-house-info');
+
+    // edit street name
+    var $streetHeader = $('.address');
+    $streetHeader.text(HOUSEDATADICTIONARY[index]['street']);
+
+
+    // // edit city/state name
+    var $cityStateHeader = $('.address2');
+    $cityStateHeader.text(HOUSEDATADICTIONARY[index]['cityState']);
+
+    // // edit house details
+    var $sizeHeader = $('.size');
+    $sizeHeader.text(HOUSEDATADICTIONARY[index]['size']);
+
+    // // edit price
+    var $priceHeader = $('#price2');
+    $priceHeader.text(HOUSEDATADICTIONARY[index]['price']);
+};
